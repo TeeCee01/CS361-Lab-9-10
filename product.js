cc39453dc9ea4e7a2b4e1c7462a45aa4e450dd9a
TeeCee01 <thandiwechambani@gmail.com>
2026-07-23 09:45:04 +0200

    Add files via upload

---
+++ product.js
@@ -0,0 +1,13 @@
+// A Product Class 
+
+export class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  // returns the price with 10% tax added
+  withTax() {
+    return this.price + this.price * 0.10;
+  }
+}
